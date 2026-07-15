f029d413a9f6ba81e6d3f1b76079e2cb6ec393ab
zDefcon21 <antholozadarequelme@gmail.com>
2026-07-14 22:48:22 -0500

    Mejorar la clase ListaOrdinalPersonas: se agregaron comentarios explicativos en el constructor y el método toString para mayor claridad sobre el uso de la cédula como identificador en el nodo AVL.

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -5,17 +5,18 @@ import tadCola.*;
 import tadLista.*;
 
 class ListaOrdinalPersonas extends TadLista<Persona> implements Comparable<ListaOrdinalPersonas> {
-    private String cedula;
+    private String cedula; // identificador de cedula para el nodo avl
 
     public ListaOrdinalPersonas(String cedula) {
-        super(cedula);
-        this.cedula = cedula;
+        super(cedula); // nombre del tadList (cedula) para el nodo
+        this.cedula = cedula; // guardamos la cedula
     }
 
     public String getCedula() {
         return this.cedula;
     }
 
+    // el avl toma como referencia la cedula del nodo para ordenar los nodos
     @Override
     public int compareTo(ListaOrdinalPersonas p) {
         if (getCedula().compareTo(p.getCedula()) > 0) {
@@ -27,6 +28,7 @@ class ListaOrdinalPersonas extends TadLista<Persona> implements Comparable<Lista
         return 0;
     }
 
+    // informacion basica del nodo
     @Override
     public String toString() {
         return "Cedula: " + this.getCedula() + "\n" + super.toString();
