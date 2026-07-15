96a1463e8755ab62b17c7a2c03ff841e961dd9c7
zDefcon21 <antholozadarequelme@gmail.com>
2026-07-15 00:21:16 -0500

    Agregar implementación de AlgoritmosCRUD y ListaOrdinalPersonas: se creó la clase AlgoritmosCRUD con métodos para insertar en un árbol AVL y se agregó la clase ListaOrdinalPersonas con su constructor y métodos de comparación. Se eliminó la clase ListaOrdenalPersonas y se ajustó la clase Principal para utilizar las nuevas implementaciones.

---
+++ src/algoritmos/AlgoritmosCRUD.java
@@ -0,0 +1,55 @@
+package algoritmos;
+
+import arbolBinario.ArbolAVL;
+import arbolBinario.NodoArbol;
+import principal.ListaOrdenalPersonas;
+import principal.Persona;
+import tadLista.TadLista;
+
+public class AlgoritmosCRUD {
+
+    private static void insertarOrdenal(TadLista<Persona> lista, Persona persona) {
+        if (lista.esNulo() || lista.devolverClave().compareTo(persona) > 0) {
+            AlgoritmosListas.insertarAlPrincipio(lista, persona);
+        } else {
+            TadLista<Persona> aux = new TadLista<>();
+            aux.asignarReferencia(lista.devolverReferencia());
+            insertarOrdenalR(aux, persona);
+        }
+    }
+
+    private static void insertarOrdenalR(TadLista<Persona> aux, Persona persona) {
+        TadLista<Persona> siguiente = new TadLista<>();
+        siguiente.asignarReferencia(aux.devolverSiguiente());
+
+        if (siguiente.esNulo() || siguiente.devolverClave().compareTo(persona) > 0) {
+            TadLista<Persona> nuevo = new TadLista<>();
+            nuevo.crearNodo();
+            nuevo.asignarClave(persona);
+
+            nuevo.asignarReferenciaSiguiente(siguiente.devolverReferencia());
+            aux.asignarReferenciaSiguiente(nuevo.devolverReferencia());
+        } else {
+            aux.asignarReferencia(aux.devolverSiguiente());
+            insertarOrdenalR(aux, persona);
+        }
+    }
+
+    // Insertar una persona en el arbol AVL
+    public static ArbolAVL<ListaOrdenalPersonas> insertar(ArbolAVL<ListaOrdenalPersonas> arbol, Persona persona) {
+        ListaOrdenalPersonas llave = new ListaOrdenalPersonas(persona.getCedula());
+
+        NodoArbol<ListaOrdenalPersonas> nodo = arbol.buscarNodo(llave);
+
+        if (nodo == null) {
+            ListaOrdenalPersonas lista = new ListaOrdenalPersonas(persona.getCedula());
+            insertarOrdenal(lista, persona);
+            arbol.insertar(lista);
+        } else {
+            insertarOrdenal(nodo.getClave(), persona);
+        }
+
+        return arbol;
+    }
+
+}
--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -1,44 +1,30 @@
 package principal;
 
+import algoritmos.AlgoritmosCRUD;
+import algoritmos.AlgoritmosListas;
 import arbolBinario.ArbolAVL;
-import tadCola.*;
+import arbolBinario.NodoArbol;
 import tadLista.*;
-
-class ListaOrdinalPersonas extends TadLista<Persona> implements Comparable<ListaOrdinalPersonas> {
-    private String cedula; // identificador de cedula para el nodo avl
-
-    public ListaOrdinalPersonas(String cedula) {
-        super(cedula); // nombre del tadList (cedula) para el nodo
-        this.cedula = cedula; // guardamos la cedula
-    }
-
-    public String getCedula() {
-        return this.cedula;
-    }
-
-    // el avl toma como referencia la cedula del nodo para ordenar los nodos
-    @Override
-    public int compareTo(ListaOrdinalPersonas p) {
-        if (getCedula().compareTo(p.getCedula()) > 0) {
-            return 1;
-        } else if (getCedula().compareTo(p.getCedula()) < 0) {
-            return -1;
-        }
-
-        return 0;
-    }
-
-    // informacion basica del nodo
-    @Override
-    public String toString() {
-        return "Cedula: " + this.getCedula() + "\n" + super.toString();
-    }
-
-}
 
 public class Principal {
     public static void main(String[] args) {
+        ArbolAVL<ListaOrdenalPersonas> arbolAVL = new ArbolAVL<>();
+        AlgoritmosCRUD.insertar(arbolAVL, new Persona("0034", "Juan"));
+        listar(arbolAVL);
+        arbolAVL.info();
+    }
 
+    public static void listar(ArbolAVL<ListaOrdenalPersonas> arbol) {
+        System.out.println("Listado Del Arbol AVL");
+        listarR(arbol.getRaiz());
+    }
+
+    private static void listarR(NodoArbol<ListaOrdenalPersonas> raiz) {
+        if (raiz != null) {
+            listarR(raiz.getIz());
+            raiz.getClave().imprimirLista();
+            listarR(raiz.getDe());
+        }
     }
 
 }
