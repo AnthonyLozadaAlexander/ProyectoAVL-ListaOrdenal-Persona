d2a786f9db8714e0c58b5e7b56b9d4eaa93c71cb
zDefcon21 <antholozadarequelme@gmail.com>
2026-07-14 22:46:14 -0500

    Agregar la clase ListaOrdinalPersonas con implementación de comparación y método toString

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -4,6 +4,36 @@ import arbolBinario.ArbolAVL;
 import tadCola.*;
 import tadLista.*;
 
+class ListaOrdinalPersonas extends TadLista<Persona> implements Comparable<ListaOrdinalPersonas> {
+    private String cedula;
+
+    public ListaOrdinalPersonas(String cedula) {
+        super(cedula);
+        this.cedula = cedula;
+    }
+
+    public String getCedula() {
+        return this.cedula;
+    }
+
+    @Override
+    public int compareTo(ListaOrdinalPersonas p) {
+        if (getCedula().compareTo(p.getCedula()) > 0) {
+            return 1;
+        } else if (getCedula().compareTo(p.getCedula()) < 0) {
+            return -1;
+        }
+
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Cedula: " + this.getCedula() + "\n" + super.toString();
+    }
+
+}
+
 public class Principal {
     public static void main(String[] args) {
 
