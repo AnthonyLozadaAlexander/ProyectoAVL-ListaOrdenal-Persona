a6d8b49e57477c5364f649872d7b7e6d64205669
zDefcon21 <antholozadarequelme@gmail.com>
2026-07-19 18:39:51 -0500

    Eliminar clases Algoritmos y ListaOrdenalPersonas: se eliminaron las implementaciones de las clases Algoritmos y ListaOrdenalPersonas, así como las referencias en la clase Principal, para simplificar la estructura del código y evitar redundancias. Se ajustó la lógica de inserción en el árbol AVL.

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -1,30 +1,14 @@
 package principal;
 
 import algoritmos.AlgoritmosListas;
-import algoritmosCRUD.Algoritmos;
+import arbolBinario.Arbol;
 import arbolBinario.ArbolAVL;
 import arbolBinario.NodoArbol;
 import tadLista.*;
 
 public class Principal {
     public static void main(String[] args) {
-        ArbolAVL<ListaOrdenalPersonas> arbolAVL = new ArbolAVL<>("Arbol AVL - Listas De Personas");
-        Algoritmos.insertar(arbolAVL, new Persona("0034", "Juan"));
-        listar(arbolAVL);
-        arbolAVL.info();
-    }
-
-    public static void listar(ArbolAVL<ListaOrdenalPersonas> arbol) {
-        System.out.println("Listado Del Arbol AVL");
-        listarR(arbol.getRaiz());
-    }
-
-    private static void listarR(NodoArbol<ListaOrdenalPersonas> raiz) {
-        if (raiz != null) {
-            listarR(raiz.getIz());
-            raiz.getClave().imprimirLista();
-            listarR(raiz.getDe());
-        }
+        TadLista<Persona> lista = new TadLista<>("Lista Personas");
     }
 
 }
